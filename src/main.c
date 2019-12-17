4a8b9afceaf31b665b5423c8b0dccb107148fafa
Adrian Macal <adrian.macal@flixbus.com>
2019-12-17 14:07:33 +0100

    Remove usage of global variables.

--- src/main.c
+++ src/main.c
@@ -3,60 +3,77 @@
 #include <curl/curl.h>
 #include <uv.h>
 
-uv_loop_t *loop;
-uv_timer_t timer;
-CURLM *multi_handle;
+typedef struct peak_multi_context_s {
+  uv_loop_t *uv_loop;
+  uv_timer_t *uv_timer;
+  CURLM *multi_handle;
+} peak_multi_context_t;
 
-typedef struct curl_context_s {
+typedef struct peak_socket_context_s {
   uv_poll_t poll_handle;
   curl_socket_t sockfd;
-} curl_context_t;
+  CURLM *multi_handle;
+} peak_socket_context_t;
 
 int curlm_describe(char *context, int errornum) {
   printf("%s: %d, %s\n", context, errornum, curl_multi_strerror(errornum));
   return errornum;
 }
 
-size_t on_write(char *ptr, size_t size, size_t nmemb, void *userdata) {
-  printf("wrote %zu bytes\n", nmemb);
+size_t peak_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
   return nmemb;
 }
 
-int on_progress(void *clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow) {
-  return 0;
-}
+peak_socket_context_t* peak_create_socket_context(curl_socket_t sockfd, peak_multi_context_t *multi_context) {
+  peak_socket_context_t *context;
 
-curl_context_t* create_curl_context(curl_socket_t sockfd) {
-  curl_context_t *context;
-
-  context = (curl_context_t *)malloc(sizeof(*context));
+  context = (peak_socket_context_t*)malloc(sizeof(*context));
   context->sockfd = sockfd;
+  context->multi_handle = multi_context->multi_handle;
 
-  uv_poll_init_socket(loop, &context->poll_handle, sockfd);
+  uv_poll_init_socket(multi_context->uv_loop, &context->poll_handle, sockfd);
   context->poll_handle.data = context;
 
   return context;
 }
 
-void curl_close_cb(uv_handle_t *handle) {
-  curl_context_t *context = (curl_context_t*)handle->data;
-  free(context);
+peak_multi_context_t* peak_create_multi_context(uv_timer_t *uv_timer, uv_loop_t *uv_loop, CURLM *multi_handle) {
+  peak_multi_context_t *context;
+  context = (peak_multi_context_t*)malloc(sizeof(*context));
+
+  context->uv_timer = uv_timer;
+  context->uv_loop = uv_loop;
+  context->multi_handle = multi_handle;
+
+  return context;
 }
 
-void check_multi_info(CURLM *multi) {
-  CURLMsg *info;
-  CURL *easy;
-  int pending;
+void peak_uv_close_callback(uv_handle_t *handle) {
+  free((peak_socket_context_t*)handle->data);
+}
 
-  while ((info = curl_multi_info_read(multi, &pending))) {
-    printf("message: %d, pending: %d\n", info->msg, pending);
+void check_multi_info(CURLM *multi_handle) {
+  int pending;
+  char *url;
+  long code;
+
+  CURLMsg *info;
+  CURL *easy_handle;
+
+  while ((info = curl_multi_info_read(multi_handle, &pending))) {
     switch (info->msg) {
       case CURLMSG_DONE:
-        easy = info->easy_handle;
-        printf("done\n");
+        easy_handle = info->easy_handle;
 
-        curl_multi_remove_handle(multi_handle, easy);
-        curl_easy_cleanup(easy);
+        curl_easy_getinfo(easy_handle, CURLINFO_EFFECTIVE_URL, &url);
+        printf("url: %s\n", url);
+
+        curl_easy_getinfo(easy_handle, CURLINFO_RESPONSE_CODE, &code);
+        printf("code: %ld\n", code);
+
+        curl_multi_remove_handle(multi_handle, easy_handle);
+        curl_easy_cleanup(easy_handle);
+      break;
       default:
         printf("default\n");
     }
@@ -65,44 +82,39 @@ void check_multi_info(CURLM *multi) {
 
 void curl_perform(uv_poll_t *req, int status, int events) {
   int running, flags = 0;
-  curl_context_t *context = (curl_context_t*)req->data;
+  peak_socket_context_t *context = (peak_socket_context_t*)req->data;
 
   if (events & UV_READABLE) flags |= CURL_CSELECT_IN;
   if (events & UV_WRITABLE) flags |= CURL_CSELECT_OUT;
 
-  curl_multi_socket_action(multi_handle, context->sockfd, flags, &running);
-  check_multi_info(multi_handle);
+  curl_multi_socket_action(context->multi_handle, context->sockfd, flags, &running);
+  check_multi_info(context->multi_handle);
 }
 
-int socket_callback(CURL *easy, curl_socket_t socket, int action, void *userp, void *socketp) {
-
+int peak_socket_callback(CURL *easy, curl_socket_t socket, int action, void *userp, void *socketp) {
   int events = 0;
-  curl_context_t *context;
+  peak_socket_context_t *socket_context;
 
-  if (socketp) {
-    context = (curl_context_t*)socketp;
-  }
+  peak_multi_context_t *multi_context = (peak_multi_context_t*)userp;
+  if (socketp) socket_context = (peak_socket_context_t*)socketp;
 
   switch (action) {
     case CURL_POLL_IN:
     case CURL_POLL_OUT:
     case CURL_POLL_INOUT:
-      context = socketp ? context : create_curl_context(socket);
-      curl_multi_assign(multi_handle, socket, (void*) context);
+      socket_context = socketp ? socket_context : peak_create_socket_context(socket, multi_context);
+      if (!socketp) curl_multi_assign(multi_context->multi_handle, socket, (void*) socket_context);
 
       if (action != CURL_POLL_IN) events |= UV_WRITABLE;
       if (action != CURL_POLL_OUT) events |= UV_READABLE;
 
-      printf("socket started: %d\n", action);
-      uv_poll_start(&context->poll_handle, events, curl_perform);
+      uv_poll_start(&socket_context->poll_handle, events, curl_perform);
     break;
     case CURL_POLL_REMOVE:
       if (socketp) {
-        uv_poll_stop(&context->poll_handle);
-        curl_multi_assign(multi_handle, socket, NULL);
-
-        printf("socked removed\n");
-        uv_close((uv_handle_t*)&context->poll_handle, curl_close_cb);
+        uv_poll_stop(&socket_context->poll_handle);
+        curl_multi_assign(multi_context->multi_handle, socket, NULL);
+        uv_close((uv_handle_t*)&socket_context->poll_handle, peak_uv_close_callback);
       }
     break;
   }
@@ -113,52 +125,41 @@ int socket_callback(CURL *easy, curl_socket_t socket, int action, void *userp, v
 void on_timeout(uv_timer_t *timer) {
   int handles, errornum;
 
-  errornum = curl_multi_socket_action(multi_handle, CURL_SOCKET_TIMEOUT, 0, &handles);
+  peak_multi_context_t *context;
+  context = (peak_multi_context_t*)timer->data;
+
+  errornum = curl_multi_socket_action(context->multi_handle, CURL_SOCKET_TIMEOUT, 0, &handles);
   if (errornum != CURLM_OK) curlm_describe("curl_multi_socket_action", errornum);
 
-  printf("handles: %d\n", handles);
-  check_multi_info(multi_handle);
+  check_multi_info(context->multi_handle);
 }
 
-int timer_callback(CURLM *multi, long timeout_ms, void *userp) {
-  printf("timeout: %ld\n", timeout_ms);
+int peak_timer_callback(CURLM *multi, long timeout_ms, void *userp) {
+  peak_multi_context_t *context;
+  context = (peak_multi_context_t*)userp;
 
   if (timeout_ms < 0) {
-    uv_timer_stop(&timer);
+    uv_timer_stop(context->uv_timer);
     return 0;
   }
 
-  if (timeout_ms == 0) {
-    timeout_ms = 1;
-  }
+  if (timeout_ms == 0) timeout_ms = 1;
+  uv_timer_start(context->uv_timer, on_timeout, timeout_ms, 0);
 
-  uv_timer_start(&timer, on_timeout, timeout_ms, 0);
   return 0;
-}
-
-int curl_perform_transfer(CURLM *multi_handle) {
-  int running, errornum;
-
-  do {
-    errornum = curl_multi_wait(multi_handle, NULL, 0, 1000, NULL);
-    if (errornum != CURLM_OK) return curlm_describe("curl_multi_wait", errornum);
-
-    errornum = curl_multi_perform(multi_handle, &running);
-    if (errornum != CURLM_OK) return errornum;
-  } while(running);
-
-  return CURLM_OK;
 }
 
 int main(int argc, char **argv) {
 
   int curle;
+  uv_timer_t *timer;
+  CURLM *multi_handle;
+  peak_multi_context_t *multi_context;
 
-  printf("%s\n", curl_version());
-  printf("%s\n", uv_version_string());
+  printf("libcurl: %s\n", curl_version_info(CURLVERSION_NOW)->version);
+  printf("libuv: %s\n", uv_version_string());
 
-  loop = uv_default_loop();
-  uv_timer_init(loop, &timer);
+  uv_loop_t *loop = uv_default_loop();
 
   curle = curl_global_init(CURL_GLOBAL_ALL);
   if (curle != 0) return -1;
@@ -169,29 +170,32 @@ int main(int argc, char **argv) {
   multi_handle = curl_multi_init();
   if (multi_handle == NULL) return -1;
 
-  curle = curl_easy_setopt(easy_handle, CURLOPT_NOPROGRESS, 0);
-  if (curle != CURLE_OK) return -2;
+  timer = (uv_timer_t*)malloc(sizeof(*timer));
+  uv_timer_init(loop, timer);
+
+  multi_context = peak_create_multi_context(timer, loop, multi_handle);
+  timer->data = multi_context;
 
   curle = curl_easy_setopt(easy_handle, CURLOPT_URL, "https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip");
   if (curle != CURLE_OK) return -2;
 
-  curle = curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, on_write);
+  curle = curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, peak_write_callback);
   if (curle != CURLE_OK) return -2;
 
-  curle = curl_easy_setopt(easy_handle, CURLOPT_XFERINFOFUNCTION, on_progress);
-  if (curle != CURLE_OK) return -2;
-
-  curle = curl_multi_setopt(multi_handle, CURLMOPT_SOCKETFUNCTION, socket_callback);
+  curle = curl_multi_setopt(multi_handle, CURLMOPT_SOCKETFUNCTION, peak_socket_callback);
   if (curle != CURLM_OK) return -3;
 
-  curle = curl_multi_setopt(multi_handle, CURLMOPT_TIMERFUNCTION, timer_callback);
+  curle = curl_multi_setopt(multi_handle, CURLMOPT_SOCKETDATA, multi_context);
+  if (curle != CURLM_OK) return -3;
+
+  curle = curl_multi_setopt(multi_handle, CURLMOPT_TIMERFUNCTION, peak_timer_callback);
+  if (curle != CURLM_OK) return -3;
+
+  curle = curl_multi_setopt(multi_handle, CURLMOPT_TIMERDATA, multi_context);
   if (curle != CURLM_OK) return -3;
 
   curle = curl_multi_add_handle(multi_handle, easy_handle);
   if (curle != CURLM_OK) return -3;
-
-  //curle = curl_perform_transfer(multi_handle);
-  //if (curle != CURLM_OK) return curlm_describe("curl_perform_transfer", curle);
 
   uv_run(loop, UV_RUN_DEFAULT);
   curl_multi_cleanup(multi_handle);
